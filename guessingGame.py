d9ccca1a0f38fa3a8a3d90c50d139aef748bf905
Carter Valeski (Class of 2026) <261800cv@internal.salesianum.org>
2023-11-16 14:32:01 -0500

    Carter

--- guessingGame.py
+++ guessingGame.py
@@ -1,3 +1,14 @@
-def play_game():
+def main():
+    play_game()
+def loop():
     input ("NEW GAME? Y/N: ")
-    
+    if input == ("Y"):
+        play_game()
+def play_game():
+    loop()
+    #game code
+    play_game()
+
+
+if __name__ == "__main__":
+    main()
